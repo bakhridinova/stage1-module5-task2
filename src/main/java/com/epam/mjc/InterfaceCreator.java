6b2a40364b2cfef52358bfecf6a07be1cde0eee4
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-22 18:22:26 +0500

    updated InterfaceCreator.java

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,15 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return arg -> {
+            List<Integer> divided = new ArrayList<>();
+            arg.forEach(elem -> divided.add(elem / divider));
+            return divided;
+        };
     }
 }
